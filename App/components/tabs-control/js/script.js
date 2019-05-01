25ebe28bc4a825d6c899bb5f11434db7d21050e1
Dima <kobzarevdmitry@yandex.by>
2019-05-01 22:30:16 +0300

    tabs-controls component via js

--- App/components/tabs-control/js/script.js
+++ App/components/tabs-control/js/script.js
@@ -2,29 +2,43 @@ class TabsControl {
 
 	constructor (wrapper, relPath, tabNames) {
 	this.tabs = [];
-
 	this.tabNames = tabNames;
     this.path = document.URL + relPath;
     this.wrapper = wrapper;
     this.loadStyles();
     this.loadTemplate();
     this.state = {
-    	activeTabNum: 0;
+    	activeTabNum: null
     }
+    this.setActiveTab(0);
 	}
     
-    
+    setActiveTab(tabNum) {
+    	this.state.activeTabNum = tabNum;
+    	this.tabs.forEach((tab) => {
+    		tab.classList.remove('tabs-control__item--current');
+       	})
+       	this.tabs[tabNum].classList
+       	  .add('tabs-control__item--current');
+    }
+
 
 	loadTemplate () {
-		const xhr = new XMLHttpRequest();
-		const URL = this.path + 
-		  '/index.html';
-		xhr.open('GET', URL);
-		xhr.send();
-
-		xhr.onload = () => {
-          this.wrapper.innerHTML = xhr.response;
-    	}
+		const template = document.createElement('ul');
+		template.classList.add('tabs-control');
+		this.tabNames.forEach((tabName, ind) => {
+			const item = document.createElement('li');
+			this.tabs.push(item);
+			item.dataset.tabNum = ind;
+			item.classList.add('tabs-control__item');
+			item.textContent = tabName;
+			template.appendChild(item);
+		})
+		this.wrapper.innerHTML = '';
+		this.wrapper.appendChild(template);
+		template.onclick = (e) => {
+			this.setActiveTab(e.target.dataset.tabNum);
+		}
 	}
 
 	loadStyles () {
@@ -33,10 +47,9 @@ class TabsControl {
 		styles.rel = 'stylesheet';
         document.head.appendChild(styles);
 	}
-
 }
 
 const wrapper = 
   document.querySelector('.tabs-control-wrapper');
 const tabsControlExemplar = new TabsControl(wrapper,
-	'App/components/tabs-control');
+	'App/components/tabs-control', ['list', 'map', 'add form']);

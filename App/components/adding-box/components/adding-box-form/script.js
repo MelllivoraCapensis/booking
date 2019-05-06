cba5bd1315ba3f08a1b352416b5914b060afcb1f
Dima <kobzarevdmitry@yandex.by>
2019-05-06 03:26:00 +0300

    adding form via html template

--- App/components/adding-box/components/adding-box-form/script.js
+++ App/components/adding-box/components/adding-box-form/script.js
@@ -1,97 +1,28 @@
 class AddingBoxForm {
 
-	constructor (wrapper, inputs) {
+	constructor (wrapper, relPath) {
 		this.wrapper = wrapper;
-		this.inputs = inputs;
-        
-        this.state = {};
+        this.path = relPath;
+        this.state = {
+        	features: []
+        };
 
 		this.init();
 	}
 
 	init () {
 		this.build();
-		this.addHandlers();
 	}
 
 	build () {
-		this.form = document.createElement('form');
-		this.form.classList.add('adding-box__form');
-		this.form.classList.add('adding-form');
-		this.wrapper.appendChild(this.form);
-
-		this.inputs.filter((inputObject) => inputObject.type != 'checkbox')
-		    .forEach((inputObject) => {
-			    this.appendInput(inputObject, this.form);
-		    })
-        
-		this.featuresBox = document.createElement('fieldset');
-		this.featuresBox.classList.add('adding-box__features');
-		this.featuresBox.classList.add('features-box');
-		this.form.insertBefore(this.featuresBox, 
-			this.form.querySelector('.adding-box__submit'));
-
-        this.inputs.filter((inputObject) => inputObject.type == 'checkbox')
-            .forEach((inputObject) => {
-            	this.appendInput(inputObject, this.featuresBox);
-            })
-
-		this.appendInput({tag: 'button', type: 'submit', name: 'submit', 
-			value: 'send', label: ''}, this.form);
-	}
-
-	appendInput (inputObject, container) {
-
-        const input = document.createElement(inputObject.tag);
-       
-        switch (inputObject.tag) {
-        	case 'input':
-        		input.type = inputObject.type;
-        		if(input.type == 'number')
-        		{
-        			input.min = inputObject.min;
-        			input.max = inputObject.max;
-        		}
-        		break;
-    		case 'select':
-    		    inputObject.options.forEach((option) => {
-        		const item = document.createElement('option');
-        		input.appendChild(item);
-        		item.value = Object.keys(option)[0];
-        		item.textContent = option[item.value];
-        	    })
-	    		break;
-    		case 'button':
-    		    input.textContent = inputObject.value;
-	    		break;
-	        case 'textarea':
-	            input.minLength = inputObject.minlength;
-	            input.maxLength = inputObject.maxlength;
-        	default:
-        		// statements_def
-        		break;
-        }
-
-        const classPrefix = 
-		    (input.type == 'checkbox') ? 'features-box__' : 'adding-form__';
-        console.log(classPrefix);
-        input.classList.add(classPrefix + inputObject.name);
-
-		const label = document.createElement('label');
-		label.classList.add(classPrefix + inputObject.name + '-label');
-       
-        input.name = inputObject.name;
-        input.required = true;
-
-
-        const labelText = document.createElement('span');
-        labelText.classList.add(classPrefix + 
-            inputObject.name + '-label-text');
-        labelText.textContent = inputObject.label;
-
-        label.appendChild(labelText);
-        label.appendChild(input);
-		container.appendChild(label);
+		const xhr = new XMLHttpRequest();
+		xhr.open('GET', this.path + '/index.html');
+		xhr.send();
+		xhr.onload = (e) => {
+			this.wrapper.innerHTML = xhr.response;
+			this.form = this.wrapper.querySelector('form.adding-box__form');
+			this.addHandlers();
+		}
 	}
 
 	addHandlers () {
@@ -100,6 +31,39 @@ class AddingBoxForm {
 			if(!this.isValid())
 				return;
 		}
+		this.addFeaturesClickHandler();
+		
+	}
+
+	addFeaturesClickHandler () {
+		this.form.querySelectorAll('.features-box div')
+		    .forEach((item) => {
+		    	const feature = {
+		    		name: item.dataset.name,
+		    		checked: false,
+		    		element: item
+		    	};
+		    	feature.element.querySelector('img').
+		    	    onclick = (e) => {
+		    	    	e.preventDefault();
+		    	    }
+		    	const toggleFunction = (e) => {
+		    		feature.checked = !feature.checked;
+                    feature.element.classList.toggle(
+                    	'features-box__item--checked');
+		    	}
+
+		    	feature.element.onclick = toggleFunction;
+
+		    	feature.element.onkeydown = (e) => {
+		    		if(e.key == 'Enter')
+		    		toggleFunction();
+
+		    	}
+		    	this.state.features.push(feature);
+		    })
+		
+
 	}
 
 	isValid () {
